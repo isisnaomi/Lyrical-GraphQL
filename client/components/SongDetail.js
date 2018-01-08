c3f12bbda913b68c9e74c786d27def82b61cf12d
Isis Naomi Ramirez <isisnaomi@gmail.com>
2018-01-08 17:53:45 -0600

    Navigation between screens

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import fetchSong from '../queries/fetchSong';
+import { Link } from 'react-router';
 
 class SongDetail extends Component {
   render(){
     const { song } = this.props.data;
 
-    if( !song ){ return <div>Loading...</div>; } 
+    if( !song ){ return <div>Loading...</div>; }
 
     return(
       <div>
+        <Link to="/">Back</Link>
         <h3>{ song.title }</h3>
       </div>
     );
